4a7d13ee7330d08f664ea83a532cd66bf3623c3b
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-15 22:48:13 -0300

    Simple Create Form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,5 +22,10 @@ namespace SalesWebMvc.Services
             return _contex.Seller.ToList();
         }
 
+        public void Insert(Seller obj)
+        {
+            _contex.Add(obj);
+            _contex.SaveChanges();
+        }
     }
 }
