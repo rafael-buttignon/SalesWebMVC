5c1aefad2bdfc80580c44053ea3c0bbd5a683a3c
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-15 22:25:56 -0300

    SellersService and basic find

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _contex;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _contex = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _contex.Seller.ToList();
+        }
+
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -36,6 +37,7 @@ namespace SalesWebMvc
                     builder => builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
